9cc35d7c23a23d70107417526393180182e7de82
lhxcccccccccc <3302269842@qq.com>
2024-06-05 18:20:18 +0800

    [Serverless官网]: detail页面详情接口适配v3

---
+++ services/downloadV3Plugin.js
@@ -0,0 +1,20 @@
+// V3 版本点击下载方法
+function downloadV3Plugin(name) {
+    // 获取 Package 的最新 GET
+    // https://server-serverlgistry-v-awljqvnszb.cn-hangzhou.fcapp.run/v3/packages/{package-name}/release/latest
+    var base_url = BASE_V3_URL
+     var url = base_url + '/packages/'+name+'/release/latest'
+     _Get(url, function (res) {
+       var result = res.body||res.Response;
+       if (result && result.zipball_url) {
+         const link = document.createElement('a');
+         link.setAttribute('href', result.zipball_url);
+         link.setAttribute('target', "_blank");
+         document.body.appendChild(link);
+         link.click();
+         document.body.removeChild(link);      
+       }
+       return ;
+
+     }, 'isAllData')
+}
