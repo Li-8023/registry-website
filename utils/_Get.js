eed780ceaa648a69f357654c8bfe1dc6feb9e9ca
lhxcccccccccc <3302269842@qq.com>
2024-06-06 18:31:43 +0800

    [servers官网]: search接口增加参数&platform=3

--- utils/_Get.js
+++ utils/_Get.js
@@ -38,3 +38,21 @@ function _Get(url, callback, type) {
     );
     xmlhttp.send();
 }
+
+
+// _GetJSON: 方法传递json参数,默认返回接口所有数据
+function _GetJSON(url, callback){
+  const xmlhttp = window.XMLHttpRequest
+  ? new XMLHttpRequest()
+  : new ActiveXObject("Microsoft.XMLHTTP");
+xmlhttp.open("GET", url, false);
+xmlhttp.onreadystatechange = function () {
+  if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+    var result = JSON.parse(xmlhttp.responseText);
+    callback&&callback(result)
+    return ;
+  }
+};
+xmlhttp.setRequestHeader("Content-type", "application/json");
+xmlhttp.send();
+}
