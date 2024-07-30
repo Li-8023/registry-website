8cbb6145871e5d8cd79e30c2d392e02a09853943
Li He <lihe5@asu.edu>
2024-07-30 17:27:45 +0800

    finish the card item Signed-off-by: Li-8023 <lihe5@asu.edu>
    Signed-off-by: Li-8023 <lihe5@asu.edu>

--- app/application/page.js
+++ app/application/page.js
@@ -1,16 +1,97 @@
+"use client";
+
+import React, { useEffect, useState } from "react";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
+import CardItem from "../components/card/CardItem"; 
+
 const ApplicationPage = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    // Fake data for testing purposes
+    const fakeData = [
+      {
+        name: "Plugin One",
+        package_type: "v2",
+        delayTime: 0.2,
+        download: 100,
+        published_at: "2023-01-01",
+        version: "1.0.0",
+        description: "This is a description for Plugin One.",
+        zipball_url: "http://example.com/plugin-one.zip",
+      },
+      {
+        name: "Plugin Two",
+        package_type: "v1",
+        delayTime: 0.3,
+        download: 200,
+        published_at: "2023-02-01",
+        version: "2.0.0",
+        description: "This is a description for Plugin Two.",
+        zipball_url: "",
+      },
+      {
+        name: "Plugin Three",
+        package_type: "v2",
+        delayTime: 0.4,
+        download: 150,
+        published_at: "2023-03-01",
+        version: "1.2.0",
+        description: "This is a description for Plugin Three.",
+        zipball_url: "http://example.com/plugin-three.zip",
+      },
+      {
+        name: "Plugin Four",
+        package_type: "v1",
+        delayTime: 0.5,
+        download: 250,
+        published_at: "2023-04-01",
+        version: "3.0.0",
+        description: "This is a description for Plugin Four.",
+        zipball_url: "",
+      },
+      // Add more items as needed
+    ];
+
+    // Simulate data fetching
+    setTimeout(() => {
+      setData(fakeData);
+    }, 1000); // Simulate a network delay
+  }, []);
+
+  const [sortedData, setSortedData] = useState(data);
+
+  useEffect(() => {
+    setSortedData(data);
+  }, [data]);
+
+  const handleSort = (criteria) => {
+    const sorted = [...data].sort((a, b) => {
+      if (criteria === "name") {
+        return a.name.localeCompare(b.name);
+      }
+      if (criteria === "download") {
+        return b.download - a.download;
+      }
+      if (criteria === "date") {
+        return new Date(b.published_at) - new Date(a.published_at);
+      }
+      return 0;
+    });
+    setSortedData(sorted);
+  };
+
   return (
     <div>
       <Header />
 
       {/* Banner start */}
-      <section class="breadcrumb-area">
-        <div class="container">
-          <div class="content">
-            <h2 class="breadd wow fadeInUp">应用 </h2>
-            <ul class="breadcrumb-list wow fadeInUp">
+      <section className="breadcrumb-area">
+        <div className="container">
+          <div className="content">
+            <h2 className="breadd wow fadeInUp">应用 </h2>
+            <ul className="breadcrumb-list wow fadeInUp">
               <li>
                 <a href="/">首页 /</a>
               </li>
@@ -20,9 +101,48 @@ const ApplicationPage = () => {
         </div>
       </section>
       {/* Banner end */}
-      <div className="mb-14">
 
-      </div>
+      {/* Main Content Section */}
+      <section className="main-content-section">
+        <div className="container mx-auto flex flex-wrap py-12">
+          {/* Left Section */}
+          <div className="w-full md:w-7/12 lg:w-8/12">
+            <div className="row">
+              {sortedData.map((item, index) => (
+                <CardItem key={index} item={item} />
+              ))}
+            </div>
+          </div>
+
+          {/* Right Section */}
+          <div className="w-full md:w-5/12 lg:w-4/12 pl-4">
+            <div className="sorting-section bg-white p-4 rounded shadow">
+              <h4 className="mb-4">分类</h4>
+              <div className="btn-group-vertical w-full">
+                <button
+                  className="btn btn-outline-primary mb-2"
+                  onClick={() => handleSort("name")}
+                >
+                  按名称排序
+                </button>
+                <button
+                  className="btn btn-outline-primary mb-2"
+                  onClick={() => handleSort("download")}
+                >
+                  按下载量排序
+                </button>
+                <button
+                  className="btn btn-outline-primary mb-2"
+                  onClick={() => handleSort("date")}
+                >
+                  按发布时间排序
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
       <Footer />
     </div>
   );
--- app/components/card/CardItem.js
+++ app/components/card/CardItem.js
@@ -1,3 +1,119 @@
-export default function CardItem(){
-    
-}
+import React from "react";
+import Link from "next/link";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faBook,
+  faDownload,
+  faFire,
+  faCalendar,
+  faDesktop,
+} from "@fortawesome/free-solid-svg-icons";
+
+const CardItem = ({ item }) => {
+  const splitMaxLength = (str, length) => {
+    return str.length > length ? str.substring(0, length) + "..." : str;
+  };
+
+  const downloadPlugin = (name) => {
+    console.log(`Downloading plugin: ${name}`);
+  };
+
+  return (
+    <div
+      className="col-xl-6 wow fadeInUp mb-4"
+      data-wow-delay={`${item.delayTime}s`}
+      style={{
+        visibility: "visible",
+        animationDelay: "0.2s",
+        animationName: "fadeInUp",
+      }}
+    >
+      <div
+        className="themes-box shadow-lg rounded-2xl overflow-hidden"
+        style={{ overflow: "hidden" }}
+      >
+        {/* {item.package_type === "v2" && (
+          <div
+            className="ribbon"
+            data-spm-anchor-id="5176.28448578.0.i154.758e60cczu7hm5"
+          >
+            仅维护
+          </div>
+        )} */}
+        <div className="main-content p-4">
+          <h5 className="flex justify-between items-center">
+            <Link
+              href={`details.html?name=${item.name}&package_type=${item.package_type}`}
+            >
+              {splitMaxLength(item.name, 27)}
+            </Link>
+            <span className="flex items-center text-orange-500">
+              <FontAwesomeIcon icon={faFire} className="mr-1" />
+              {item.download}
+            </span>
+          </h5>
+          <div className="last-part mt-8">
+            <div className="right" style={{ width: "100%" }}>
+              <p className="text" style={{ width: "100%" }}>
+                {item.published_at && (
+                  <>
+                    <FontAwesomeIcon icon={faCalendar} /> {item.published_at}
+                  </>
+                )}
+                {item.published_at && item.version && (
+                  <>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;</>
+                )}
+                {item.version && (
+                  <>
+                    <FontAwesomeIcon icon={faDesktop} /> V{item.version}
+                  </>
+                )}
+              </p>
+              <p
+                className="text-flow-ellipsis-multiple mt-2"
+                style={{ height: "70px" }}
+              >
+                {item.description}
+              </p>
+            </div>
+          </div>
+          <div className="past-part mt-4 ml-7">
+            <center>
+              <div className="button flex space-x-10">
+                <Link
+                  href={`details.html?name=${item.name}&package_type=${item.package_type}`}
+                >
+                  <button
+                    className="preButton btn btn-outline-primary"
+                    style={{ backgroundColor: "#6676fa", color: "white" }}
+                  >
+                    <FontAwesomeIcon icon={faBook} /> 查看详情
+                  </button>
+                </Link>
+                {item.zipball_url ? (
+                  <a
+                    href={item.zipball_url}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="download btn btn-outline-secondary ml-2"
+                  >
+                    <FontAwesomeIcon icon={faDownload} /> 下载组件
+                  </a>
+                ) : (
+                  <button
+                    onClick={() => downloadPlugin(item.name)}
+                    className="download btn btn-outline-secondary ml-2"
+                  >
+                    <FontAwesomeIcon icon={faDownload} /> 下载组件
+                  </button>
+                )}
+              </div>
+            </center>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CardItem;
